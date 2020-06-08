2595e2cb8324ba95b38e610c242bec2de6bbaa0f
kurokuman <kurosawa.tomoyuki0812@gmail.com>
2020-06-09 00:25:29 +0900

    add syncMutex

---
+++ defaultSelection/defaultSelection.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tick := time.Tick(100 * time.Microsecond)
+	boom := time.After(500 * time.Microsecond)
+
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick")
+		case <-boom:
+			fmt.Println("boom")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Microsecond)
+		}
+
+	}
+}
